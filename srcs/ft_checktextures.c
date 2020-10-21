222de23f5e824631873ace9eea100ac50331dd96
Garreth Verhelpen <garreth@Garreths-MBP.lan>
2020-10-21 17:16:38 +0200

    Modifications checktextures

--- srcs/ft_checktextures.c
+++ srcs/ft_checktextures.c
@@ -3,17 +3,14 @@
 int	ft_textNO(t_list *list)
 {
 	int		a;
-	char	*wall;
 
 	a = 0;
-	wall = 0;
 	if (list->textNO[a] == 'N' && list->textNO[a + 1] == 'O')
 		a += 2;
 	while (list->textNO[a] != '\0' && list->textNO[a] == ' ')
 		a++;
-	wall = &list->textNO[a];
-	if (!(list->imgNO = mlx_xpm_file_to_image(list->mlx, wall,
-        &list->texWidth, &list->texHeight)))
+	if (!(list->imgNO = mlx_xpm_file_to_image(list->mlx,
+		&list->textNO[a], &list->texWidth, &list->texHeight)))
 	{
 		write(1, "Error\nPath texture\n", 19);
 		return (-1);
@@ -24,17 +21,14 @@ int	ft_textNO(t_list *list)
 int	ft_textSO(t_list *list)
 {
 	int		a;
-	char	*wall;
 
 	a = 0;
-	wall = 0;
 	if (list->textSO[a] == 'S' && list->textSO[a + 1] == 'O')
 		a += 2;
 	while (list->textSO[a] != '\0' && list->textSO[a] == ' ')
 		a++;
-	wall = &list->textSO[a];
-	if (!(list->imgSO = mlx_xpm_file_to_image(list->mlx, wall, &list->texWidth,
-        &list->texHeight)))
+	if (!(list->imgSO = mlx_xpm_file_to_image(list->mlx,
+		&list->textSO[a], &list->texWidth, &list->texHeight)))
 	{
 		write(1, "Error\nPath texture\n", 19);
 		return (-1);
@@ -45,17 +39,14 @@ int	ft_textSO(t_list *list)
 int	ft_textWE(t_list *list)
 {
 	int		a;
-	char	*wall;
 
 	a = 0;
-	wall = 0;
 	if (list->textWE[a] == 'W' && list->textWE[a + 1] == 'E')
 		a += 2;
 	while (list->textWE[a] != '\0' && list->textWE[a] == ' ')
 		a++;
-	wall = &list->textWE[a];
-	if (!(list->imgWE = mlx_xpm_file_to_image(list->mlx, wall, &list->texWidth,
-        &list->texHeight)))
+	if (!(list->imgWE = mlx_xpm_file_to_image(list->mlx,
+		&list->textWE[a], &list->texWidth, &list->texHeight)))
 	{
 		write(1, "Error\nPath texture\n", 19);
 		return (-1);
@@ -66,17 +57,14 @@ int	ft_textWE(t_list *list)
 int	ft_textEA(t_list *list)
 {
 	int		a;
-	char	*wall;
 
 	a = 0;
-	wall = 0;
 	if (list->textEA[a] == 'E' && list->textEA[a + 1] == 'A')
 		a += 2;
 	while (list->textEA[a] != '\0' && list->textEA[a] == ' ')
 		a++;
-	wall = &list->textEA[a];
-	if (!(list->imgEA = mlx_xpm_file_to_image(list->mlx, wall, &list->texWidth,
-        &list->texHeight)))
+	if (!(list->imgEA = mlx_xpm_file_to_image(list->mlx,
+		&list->textEA[a], &list->texWidth, &list->texHeight)))
 	{
 		write(1, "Error\nPath texture\n", 19);
 		return (-1);
